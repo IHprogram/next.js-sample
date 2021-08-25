a984e2d64f8631f9e0a3f8342a9e61a5ea8771b9
IHprogram <ihm20202020@gmail.com>
2021-08-25 17:13:14 +0900

    Linkコンポーネントの使い方を学習

--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,15 @@ export default function Layout({ children, title = "HP by Nextjs" }) {
 
       <p>Hello Next.js</p>
       <header>
-        <nav className="bg-gray-800 w-screen"></nav>
+        <nav className="bg-gray-800 w-screen">
+          <Link href="/">
+            <div className="flex items-center pl-8 h-14">
+              <div className="flex space-x-4">
+                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Home</a>
+              </div>
+            </div>
+          </Link>
+        </nav>
       </header>
       <main className="flex flex-1 justify-center items-center flex-col w-screen">
         {/* childrenの中身を表示 */}
