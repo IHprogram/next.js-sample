7f53499e1599ff466867ad9f67bfcab995491e4f
IHprogram <ihm20202020@gmail.com>
2021-08-26 16:25:41 +0900

    画像を適用

--- pages/contact.js
+++ pages/contact.js
@@ -4,8 +4,21 @@ import Image from 'next/image';
 const Contact = () => {
   return (
     <div>
-      <Layout title="About">
-        <p className="font-bold">Contact info</p>
+      <Layout title="Contact">
+        <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+          <div className="mt-4">
+            <p className="font-bold">Contact info</p>
+          </div>
+          <div className="flex justify-center mt-4">
+            <Image
+              className="rounded-full"
+              src="/user.png"
+              width={60}
+              height={60}
+              alt="User"
+            />
+          </div>
+        </div>
       </Layout>
     </div>
   )
